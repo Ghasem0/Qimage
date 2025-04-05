7bc14f81095778569fc45ecee24934173fb74b9a
Qasem Y <ghasemyalame06@gmail.com>
2025-04-05 14:46:22 +0430

    add dockstring to class methods

--- Qimage.py
+++ Qimage.py
@@ -10,24 +10,25 @@ class Qimage:
     def __init__(self) -> None:
         self.SUPPORT_EXTENTIONS = ('.jpg', '.png', '.jpeg', '.webp',)
         self.DIRECTORIES = ('comb_in', 'comb_out', 'crop_in', 'crop_out', 'catch',)
+        self.DEFUALT_PIECE_HEIGHT = 8000
+        self.MIN_HEIGHT_COMBINE = 16000
         self.count = 0
-        self.comb_cont = 0
+        self.comb_count = 0
 
     def dir_exist(self, directory_list):
+        '''insure requirement directories exist.'''
         for directory in directory_list:
-            if not os.path.exists(directory):
-                os.makedirs(directory, exist_ok=True) 
-            else:...
+            os.makedirs(directory, exist_ok=True) #insure directoies exist
 
     def get_img_size(self, path:str)->int:
-        img = Image.open(path)
-        width, height = img.size
-        img.close()
-        return width, height
+        '''get width, height of image'''
+        with Image.open(path) as img:
+            width, height = img.size
+            return width, height
 
     def get_images_path(self, input_folder:str)-> list:
-        folder_pathes =os.listdir(input_folder)
-        folder_pathes.sort()
+        '''get all images path from folder'''
+        folder_pathes =sorted(os.listdir(input_folder))
         images_path = []
         for path in folder_pathes :
             if path.lower().endswith(self.SUPPORT_EXTENTIONS):
@@ -45,52 +46,60 @@ class Qimage:
             exit()
 
     def resize_hight(self, piece_height:int, height:int)->tuple:
+        '''if last image is smaller than 1000px, reset other images height to last image become 1000px or more'''
         deficit = 1000 - (height % piece_height)
         piece_height -= math.ceil(deficit) 
         remainder = height % piece_height
         return piece_height, remainder
 
-    def split_image(self, path:str, output_folder:str, resize_height=True, piece_hight:int=8000)->tuple:
-        img = Image.open(path)
-        width, height = img.size
-        riminder = height % piece_hight
-        
-        if 0 < riminder < 1000 and resize_height:
-            piece_hight, riminder = self.resize_hight(piece_hight, height )
+    def split_image(self, path:str, output_folder:str, resize_height=True, piece_hight=None)->tuple:
+        '''crop an image by 8000px height'''
+        if piece_hight == None:
+            piece_hight = self.DEFUALT_PIECE_HEIGHT
 
-        piece_count = height // piece_hight
-        
-        img_new_path=[]
-        for i in range(piece_count):
-            top = i * piece_hight
-            bottom = (i+1) * piece_hight
-            piece = img.crop((0, top, width, bottom,))
+        with Image.open(path) as img :
+            width, height = img.size
+            remainder = height % piece_hight
             
-            number = self.count + 1 if self.count >= 9 else f'0{self.count + 1}'
-            out_path = os.path.join(output_folder, f'pic_{number}.png')
-            img_new_path.append(out_path)
-            self.count += 1
+            if 0 < remainder < 1000 and resize_height: # if last image smaller than 1000px
+                piece_hight, remainder = self.resize_hight(piece_hight, height )
 
-            piece.save(out_path, 'PNG')
-            print(f"pic {number} saved!")
+            piece_count = height // piece_hight
+            
+            img_new_path=[] # created images path for return
+            for i in range(piece_count):
+                top = i * piece_hight
+                bottom = (i+1) * piece_hight
+                piece = img.crop((0, top, width, bottom,))
+                
+                number = self.count + 1 if self.count >= 9 else f'0{self.count + 1}'
+                out_path = os.path.join(output_folder, f'pic_{number}.png')
+                img_new_path.append(out_path)
+                self.count += 1
 
-        if riminder > 0:
-            top = piece_count * piece_hight
-            bottom = height
+                piece.save(out_path, 'PNG')
+                print(f"pic {number} saved!")
 
-            last_piece = img.crop((0, top, width, bottom,))
+            if remainder > 0:
+                top = piece_count * piece_hight
+                bottom = height
 
-            number = self.count + 1 if self.count >= 9 else f'0{self.count + 1}'
-            out_path = os.path.join(output_folder, f'pic_{number}.png')
-            self.count += 1
+                last_piece = img.crop((0, top, width, bottom,))
 
-            last_piece.save(out_path, 'PNG')
-            print(f"pic {number} saved!")
-            print(f'image {colored(str(os.path.splitext(os.path.basename(path))[0]), "green")} complete')
-        
-        return img_new_path
+                number = self.count + 1 if self.count >= 9 else f'0{self.count + 1}'
+                out_path = os.path.join(output_folder, f'pic_{number}.png')
+                self.count += 1
 
-    def crop_images(self,images_path:list, output_folder:str, piece_hight:int=8000)->None:
+                last_piece.save(out_path, 'PNG')
+                print(f"pic {number} saved!")
+                print(f'crop image {colored(str(os.path.splitext(os.path.basename(path))[0]), "green")} complete')
+            
+            return img_new_path
+
+    def crop_images(self,images_path:list, output_folder:str, piece_hight=None)->None:
+        '''crop a list or tuple of images'''
+        if piece_hight == None:
+            piece_hight = self.DEFUALT_PIECE_HEIGHT
 
         for fileName in images_path :
             self.split_image(fileName, output_folder, piece_hight)
@@ -98,6 +107,7 @@ class Qimage:
         self.count = 0
 
     def combine_images(self, images_path:list, output_folder:str)->str:
+        '''combine a list of images together verticaly'''
         widths, heights = zip(*(self.get_img_size(path) for path in images_path))
         output_height = sum(heights)
         width = max(widths)
@@ -117,7 +127,11 @@ class Qimage:
         print(f"image comb_{number} saved!")
         return path
       
-    def group_image_by_length(self, images_path:list, output_folder, min_len=16000):
+    def group_image_by_length(self, images_path:list, output_folder, min_len=None):
+        '''grouping images base on minimum-length and send for combine'''
+        if min_len==None :
+            min_len = self.MIN_HEIGHT_COMBINE
+
         group_images:list = []
         sums:int = 0
         catch_img =[]
